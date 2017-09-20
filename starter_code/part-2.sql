8db72990c636fcc0cc38ae311ca9fce8262ce0d8
laurenperez <laurenashleymarks@gmail.com>
2017-09-20 10:07:27 -0700

    completed

--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,16 @@
 -- Insert Data
+--INSERT INTO owner (name, age) VALUES ('Donald', 29);
+--INSERT INTO owner (name, age) VALUES ('John', 33);
+--INSERT INTO owner (name, age) VALUES ('Jane', 43);
+--INSERT INTO owner (name, age) VALUES ('Lauren', 32);
+--INSERT INTO owner (name, age) VALUES ('Chris', 29);
+--INSERT INTO owner (name, age) VALUES ('Jordan', 27);
 
+
+-- Insert Data
+--INSERT INTO property (name, units, owner_id) VALUES ('Archstone', 20, 1);
+--INSERT INTO property (name, units, owner_id) VALUES ('Willowspring', 30, 2);
+--INSERT INTO property (name, units, owner_id) VALUES ('Brownstone', 10, 3);
+--INSERT INTO property (name, units, owner_id) VALUES ('Rubix', 60, 4);
+--INSERT INTO property (name, units, owner_id) VALUES ('Rich Tower', 50, 5);
+--INSERT INTO property (name, units, owner_id) VALUES ('The Jobson', 80, 6);
